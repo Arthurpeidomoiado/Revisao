fed00a8648dc32c455b0999aadda4c2b133ad1cc
Arthur Gabriel <arthur.s@aln.senaicimatec.edu.br>
2026-09-09 11:32:59 -0300

    versão com a primeira questão feita

---
+++ Revisao/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Versão do Node: v24.16.0  Versão do Npm: v11.13.0  O ambiente está pronto para uso!</h1>
+    <br />
+    
+    </>
+  )
+}
+
+export default App
