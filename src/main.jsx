aeeec9cd1108e1a60a992a1e49583a8718331f72
Arthur Gabriel <arthur.s@aln.senaicimatec.edu.br>
2026-09-12 18:44:23 -0300

    mensagem 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import './App.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
@@ -13,14 +13,15 @@ export function Main() {
     <main className='Main' id='sobre'>
       <h1 className='Titulo_Apresentacao'>Apresentação</h1>
       <br />
-      <p>
-        Esse trabalho foi realizado por Arthur Gabriel Cabiceira Santos. Um estudante de
-        Engenharia da Computação na universidade Senai cimatec para a matéria de Fullstack do 6
-        semestre de 2026.2 do Prof. Celso.
-      </p>
-      <br />
-      <img src="/favicon.svg" alt="Logo do projeto" />
-      <br />
+      <div className='TextoApresentacao'>
+          <div className='texto'>
+            Esse trabalho foi realizado por Arthur Gabriel Cabiceira Santos. Um estudante de
+          Engenharia da Computação na universidade Senai cimatec para a matéria de Fullstack do 6
+          semestre de 2026.2 do Prof. Celso.
+          </div>
+          <img src="../imagens/imagem_perfil.jpg" alt="Perfil" width='50%'/>
+      </div>
+      <br></br>
       <small>Qualquer detalhe de contato estará disponível no topo e na parte inferior da página.</small>
       <br />
       <a href="#section">Vá para o repertório!</a>
